2677f0f49f9aed2a16e89e669d390292d01982d4
Vishal Kumar Jha <118336483+vishaljha1@users.noreply.github.com>
2023-09-14 12:49:13 +0530

    done

---
+++ website for hotel management system/script.js
@@ -0,0 +1,78 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar')
+let navbar = document.querySelector('.navbar')
+let videoBtn = document.querySelectorAll('.vid-btn')
+
+
+window.onscroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+    var name = document.getElementById('name').value
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+
+    var data = {
+        name:name,
+        email:email,
+        password:password
+    }
+    localStorage.setItem('userData', JSON.stringify(data))
+   
+});
+var users = JSON.parse(localStorage.getItem('userData'))|| [];
+var username = document.getElementById('userName')
+username.textContent=users.name
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click',()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+
+
+// var swiper = new swiper(".review-slider", {
+//     spaceBetween: 20,
+//     loop:true
+// });
+
+// function login(){
+//     var name = document.getElementById('name').value
+//     var email = document.getElementById('email').value
+//     var password = document.getElementById('password').value
+
+//     var data = {
+//         name:name,
+//         email:email,
+//         password:password
+//     }
+//     localStorage.setItem('userData', JSON.stringify(data))
+// }
